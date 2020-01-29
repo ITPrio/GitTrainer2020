7512675b57b6a74a16e19dac493329066e81d07c
Faysal Belkhadir <f.belkhadir@outlook.de>
2020-01-29 15:24:38 +0100

    Project added

---
+++ GitTrainer2020ConsoleApp/GitTrainer2020ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTrainer2020ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
